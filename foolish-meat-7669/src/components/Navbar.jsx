064a633c19a0bc77c2de5d850b2423b4366bdef8
Abhishek Singh <sabhishek900@gmail.com>
2023-01-19 10:31:36 +0530

    navbar in progress

---
+++ foolish-meat-7669/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import {Links,NavLink} from "react-router-dom";
+import { Image, Box} from '@chakra-ui/react'
+
+
+function Navbar(){
+
+    return (
+         <Box border="1px solid black" width="100%" height="200px">
+               <Box bg='red' w='100%' p={4} ></Box>
+         </Box>
+    )
+}
+
+export default Navbar;
